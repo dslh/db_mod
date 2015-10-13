239626c7396053e78cb8eda93270feaa8e0ebeed
Doug Hammond <d.lakehammond@gmail.com>
2015-10-13 16:35:14 +0200

    v0.0.3: def_prepared/statement.as(:csv)

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+0.0.3 (2015-10-13)
+==================
+
+* Configurable method framework. Adds `def_prepared/statement.as(:csv)` - [@dslh](https://github.com/dslh)
+
+0.0.2 (2015-10-12)
+==================
+
+* Adds `def_statement` to compliment `def_prepared` - [@dslh](https://github.com/dslh).
+
+0.0.1 (2015-10-11)
+==================
+
+* Initial release - [@dslh](https://github.com/dslh).
---
+++ lib/db_mod/as.rb
@@ -0,0 +1,38 @@
+require_relative 'as/csv'
+
+module DbMod
+  # Contains coercers and other functions that allow
+  # module instance methods returning an SQL result set
+  # to be extended with additional result coercion and
+  # formatting. The normal way to access this functionality
+  # is via {DbMod::Statements::ConfigurableMethod#as},
+  # which is available when defining a statement method
+  # or prepared method:
+  #
+  #  def_statement(:a, 'SELECT a, b, c FROM foo').as(:csv)
+  #  def_prepared(:b, 'SELECT d, e, f FROM bar').as(:csv)
+  module As
+    # List of available result coercion methods.
+    # Only keys defined here are allowed as arguments
+    # to {DbMod::Statements::ConfigurableMethod#as}.
+    COERCERS = {
+      csv: DbMod::As::Csv
+    }
+
+    # Extend a method so that the SQL result set it
+    # returns will be coerced to the given type.
+    # See {COERCERS} for a list of defined coercion
+    # methods.
+    #
+    # @param mod [Module] module where the method has been defined
+    # @param name [Symbol] method name
+    # @param type [Symbol] type to which result set should be coerced
+    def self.extend_method(mod, name, type)
+      unless COERCERS.key? type
+        fail ArgumentError, "#{type} not in #{COERCERS.keys.join ', '}"
+      end
+
+      Statements.extend_method(mod, name, COERCERS[type])
+    end
+  end
+end
